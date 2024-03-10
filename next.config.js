9d202158af0d9bc17b29ade90d99888aceb716e5
akhmadshin <qwasderfasder@gmail.com>
2024-03-10 11:38:43 +0700

    Add preact https://github.com/vercel/next.js/issues/42530#issuecomment-1601317656

--- next.config.js
+++ next.config.js
@@ -5,7 +5,18 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-  }
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (!isServer) {
+      Object.assign(config.resolve.alias, {
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
