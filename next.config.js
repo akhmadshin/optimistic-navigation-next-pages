f831363ef4c43b77d3c151aed4229cd5d1209cc0
akhmadshin <qwasderfasder@gmail.com>
2024-03-10 13:44:25 +0700

    Remove preact

--- next.config.js
+++ next.config.js
@@ -6,17 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!isServer) {
-      Object.assign(config.resolve.alias, {
-        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-    return config;
-  },
 }
 
 module.exports = nextConfig
