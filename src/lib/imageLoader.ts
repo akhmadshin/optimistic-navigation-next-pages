40c59cf2320241d541dd2a146cc55b6deaf2ab0b
akhmadshin <qwasderfasder@gmail.com>
2024-03-16 23:27:02 +0700

    Minor fix

--- src/lib/imageLoader.ts
+++ src/lib/imageLoader.ts
@@ -1,5 +1,5 @@
 import { ImageLoaderProps } from 'next/image';
 
 export default function imageLoader({ src, width, quality }: ImageLoaderProps) {
-  return `http://localhost:3000${src}?w=${width}&q=${quality || 75}`
+  return `${src}?w=${width}&q=${quality || 75}`
 }
