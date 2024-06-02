85c0720d19957431960629ca3bf6f309b01a79da
akhmadshin <qwasderfasder@gmail.com>
2024-06-02 12:43:52 +0700

    Fix Link component

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -18,6 +18,7 @@ export const Link = React.forwardRef<HTMLAnchorElement, Props>(function LinkComp
     href,
     beforeTransition,
     afterTransition,
+    placeholderData,
     children,
     ...restProps
   } = props;
@@ -26,7 +27,7 @@ export const Link = React.forwardRef<HTMLAnchorElement, Props>(function LinkComp
       onClick(e);
     }
     handleOptimisticNavigation(props.href, singletonRouter, () => {
-      window.placeholderData = props.placeholderData;
+      window.placeholderData = placeholderData;
       startPageTransition();
     });
   }
